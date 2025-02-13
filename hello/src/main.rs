8e5aa1aaff8dbe1e0996356a22d44e872d83813e
AshuMishraG <aashutoshm77@gmail.com>
2025-02-13 19:30:00 +0530

    First rust cargo build

---
+++ hello/src/main.rs
@@ -0,0 +1,23 @@
+// fn main() {
+//     let a = 10;
+//     let b = 15;
+//     println!("Hello, world!, {} {}", a, b);
+// }
+
+fn main() {
+    let unsigned: u8 = 10;
+    let signed: i8 = -10;
+    let float: f32 = 1.2;
+
+    println!("unsigned: {} sign: {} float: {}", unsigned, signed, float);
+
+    let letter = "c";
+    let emoji = "\u{1F600}";
+
+    println!("letter: {}, emoji: {}", letter, emoji);
+
+    let is_true = true;
+
+    println!("isTrue: {}", is_true);
+}
+
