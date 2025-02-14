6f11ce7ad2b9311f99c036b8daa48d9507140e8a
AshuMishraG <aashutoshm77@gmail.com>
2025-02-14 07:14:17 +0530

    array in rust

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,20 +4,28 @@
 //     println!("Hello, world!, {} {}", a, b);
 // }
 
+// fn main() {
+//     let unsigned: u8 = 10;
+//     let signed: i8 = -10;
+//     let float: f32 = 1.2;
+//
+//     println!("unsigned: {} sign: {} float: {}", unsigned, signed, float);
+//
+//     let letter = "c";
+//     let emoji = "\u{1F600}";
+//
+//     println!("letter: {}, emoji: {}", letter, emoji);
+//
+//     let is_true = true;
+//
+//     println!("isTrue: {}", is_true);
+// }
+
 fn main() {
-    let unsigned: u8 = 10;
-    let signed: i8 = -10;
-    let float: f32 = 1.2;
+    let arr: [u8; 3] = [1, 2, 3];
+    let other_arr: [u8; 5] = [100; 5];
 
-    println!("unsigned: {} sign: {} float: {}", unsigned, signed, float);
+    println!("index: {}, length {}", arr[0], other_arr.len());
 
-    let letter = "c";
-    let emoji = "\u{1F600}";
-
-    println!("letter: {}, emoji: {}", letter, emoji);
-
-    let is_true = true;
-
-    println!("isTrue: {}", is_true);
+    println!("{:?}", other_arr);
 }
-
