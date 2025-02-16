85306ecff371e864d12835122c0ae51c42032f18
AshuMishraG <aashutoshm77@gmail.com>
2025-02-16 06:11:14 +0530

    Enum

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -202,12 +202,21 @@
 // Enum
 
 fn main() {
-    let a: MyEnum = MyEnum::A;
-    let b: MyEnum = MyEnum::B(5);
-    let c: MyEnum = MyEnum::C { x: 10, y: 20 };
+    let a = MyEnum::A;
+    let b = MyEnum::B(5);
+    let c = MyEnum::C { x: 10, y: 20 };
+
     println!("{:?}", a);
     println!("{:?}", b);
     println!("{:?}", c);
+
+    if let MyEnum::B(val) = b {
+        println!("{}", val);
+    }
+
+    if let MyEnum::C { x, y } = c {
+        println!("{} {}", x, y);
+    }
 }
 
 #[derive(Debug)]
