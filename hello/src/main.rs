82abd38e3e196f57f352db9ecac7350f22ce70ef
AshuMishraG <aashutoshm77@gmail.com>
2025-02-16 15:11:25 +0530

    Array

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -201,27 +201,38 @@
 
 // Enum
 
+// fn main() {
+//     let a = MyEnum::A;
+//     let b = MyEnum::B(5);
+//     let c = MyEnum::C { x: 10, y: 20 };
+//
+//     println!("{:?}", a);
+//     println!("{:?}", b);
+//     println!("{:?}", c);
+//
+//     if let MyEnum::B(val) = b {
+//         println!("{}", val);
+//     }
+//
+//     if let MyEnum::C { x, y } = c {
+//         println!("{} {}", x, y);
+//     }
+// }
+//
+// #[derive(Debug)]
+// enum MyEnum {
+//     A,
+//     B(i32),
+//     C { x: i32, y: i32 },
+// }
+
+// Vector
+
 fn main() {
-    let a = MyEnum::A;
-    let b = MyEnum::B(5);
-    let c = MyEnum::C { x: 10, y: 20 };
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
-
-    if let MyEnum::B(val) = b {
-        println!("{}", val);
-    }
-
-    if let MyEnum::C { x, y } = c {
-        println!("{} {}", x, y);
-    }
-}
-
-#[derive(Debug)]
-enum MyEnum {
-    A,
-    B(i32),
-    C { x: i32, y: i32 },
+    let mut vec: Vec<i64> = vec![1, 2, 3, 4, 5];
+    vec.len();
+    vec[0];
+    vec.push(6);
+    vec.remove(0);
+    println!("{:?}", vec);
 }
